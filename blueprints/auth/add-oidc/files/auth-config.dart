266d2bcf1d3a62b2dff5a841e9d36ad3a9eb771f
SCHMOEGER <Dennis@2Hero.dev>
2026-03-17 13:39:44 +0100

    Flutter in-cluster dev mode, add-oidc login screen, domain fix
    - Flutter app runs in-cluster using ghcr.io/cirruslabs/flutter:stable
      instead of host-forward mode (no local Flutter SDK needed)
    - bin/dev: build-install-watch cycle via ADB over host.docker.internal
    - bin/init: install x86 multiarch libs on ARM64 for Android SDK tools
    - k8s manifest: cache volumes for pub/gradle, env for ADB server address
    - add-oidc blueprint: write main.dart with login screen, add emulator prereq,
      add appAuthRedirectScheme to build.gradle
    - Fix auth-config.dart domain: bluetext.localhost not local.bluetext.io

--- blueprints/auth/add-oidc/files/auth-config.dart
+++ blueprints/auth/add-oidc/files/auth-config.dart
@@ -1,11 +1,11 @@
 class AuthConfig {
-  static const issuer = 'http://curity.{{namespace}}.local.bluetext.io';
+  static const issuer = 'http://curity.{{namespace}}.bluetext.localhost';
   static const authorizationEndpoint =
-      'http://curity.{{namespace}}.local.bluetext.io/oauth/v2/oauth-authorize';
+      'http://curity.{{namespace}}.bluetext.localhost/oauth/v2/oauth-authorize';
   static const tokenEndpoint =
-      'http://curity.{{namespace}}.local.bluetext.io/oauth/v2/oauth-token';
+      'http://curity.{{namespace}}.bluetext.localhost/oauth/v2/oauth-token';
   static const clientId = 'flutter-app';
   static const redirectUri = 'com.bluetext.app://callback';
   static const scopes = ['openid', 'profile', 'email', 'roles'];
-  static const apiBaseUrl = 'http://kong.{{namespace}}.local.bluetext.io';
+  static const apiBaseUrl = 'http://kong.{{namespace}}.bluetext.localhost';
 }
